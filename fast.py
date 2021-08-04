73a7ef14e2381b5a8654a6fe8605f1e6a4ef597e
seijikojima <skojima@asr.info.gifu-u.ac.jp>
2021-08-04 18:25:01 +0900

    get by query parameter

--- fast.py
+++ fast.py
@@ -5,8 +5,9 @@ from starlette.responses import FileResponse
 app = FastAPI()
 
 @app.get('/')
-def get_main_page():
-    return FileResponse('index.html')
+def get_main_page(ean):
+    print(ean)
+    return FileResponse('get_book_information.html')
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=5000)
